60df72b9f25138300158f98a177c8bb5e659e8f3
Douglas Spencer <douglasdevelops@gmail.com>
2017-08-29 20:56:04 -0400

    Initial Commit

---
+++ CameraBareBones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraBareBones
+//
+//  Created by Douglas Spencer on 8/29/17.
+//  Copyright © 2017 Douglas Spencer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
